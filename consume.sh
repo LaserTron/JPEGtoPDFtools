e78ac9abbb90fb3e4b69b0338ef8ee0d028ee12c
Nicholas Touikan <nicholas.touikan@gmail.com>
2017-03-08 10:53:49 -0500

    archive instead of delete
    The consume script now archives the content of the inbox. This is useful for debugging.

--- consume.sh
+++ consume.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 outputname=$1
 sourcedir="ENTER-REMOVABLE-MEDIA-DIRECTORY-HERE"
+dt="`date +"%Y-%m-%d %R"`"
 targetdir="./inbox"
 echo "Moving images from media"
 mv -v $sourcedir/* $targetdir
 ./jpgstopdfs.sh $outputname
-echo "emptying inbox"
-rm $targetdir/*
+echo "emptying and archiving inbox"
+mkdir "./archive/$dt"
+mv $targetdir/* "./archive/$dt"
