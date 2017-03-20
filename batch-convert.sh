566bcbe196a2cbc46fbee9d831d465443d093f04
Nicholas Touikan <nicholas.touikan@gmail.com>
2017-03-20 12:47:16 -0400

    Batch convert
    Added a batch-convert script

---
+++ batch-convert.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for i in ./batch/*
+do
+    doc=`basename $i`
+    echo "Creating $doc..."
+    echo "consuming directory"
+    mv "$i"/* ./imgtopdf/
+    rm -d "$i"
+    sh jpgstopdfs.sh "$doc"
+done
